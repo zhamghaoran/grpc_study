f793347a7632283089bf6c7c9fe704a968121d2a
jjking <2017929959@qq.com>
2023-07-06 17:41:19 +0800

    ""

--- hello-client/main.go
+++ hello-client/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"grpc/hello-client/proto"
+)
+
+func main() {
+	// 连接创建
+	conn, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	defer conn.Close()
+	if err != nil {
+		panic(err)
+	}
+	//建立连接
+	client := proto.NewSayHelloClient(conn)
+	// 执行rpc的调用,远程的方法
+	res, _ := client.SayHello(context.Background(), &proto.HelloRequest{RequestName: "123"})
+	fmt.Println(res.GetResponseMsg())
+
+}
--- hello-server/main.go
+++ hello-server/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	pb "grpc/hello-server/proto"
+	"net"
+)
+
+type server struct {
+	pb.UnimplementedSayHelloServer
+}
+
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{ResponseMsg: "hello" + req.RequestName}, nil
+}
+
+func main() {
+	//开启端口监听
+	listen, _ := net.Listen("tcp", ":9090")
+	// 创建一个grpc 服务
+	grpcServer := grpc.NewServer()
+	// 在grpc当中注册服务
+	pb.RegisterSayHelloServer(grpcServer, &server{})
+
+	// 启动服务
+	err := grpcServer.Serve(listen)
+	if err != nil {
+		panic(err)
+	}
+
+}
